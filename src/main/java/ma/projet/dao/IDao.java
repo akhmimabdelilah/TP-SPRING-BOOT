7ef2877fabb9705cb4743ed0f99aa55571b21b83
BrightProgrammer7 <e-akhmim.a@ucd.ma>
2023-11-04 15:27:26 +0000

    school

--- src/main/java/ma/projet/dao/IDao.java
+++ src/main/java/ma/projet/dao/IDao.java
@@ -2,13 +2,16 @@ package ma.projet.dao;
 
 import java.util.List;
 
-public interface IDao <T> {
-	
-	T create (T o);
-	boolean delete (T o);
-	T update (T o);
+public interface IDao<T> {
+
+	T create(T o);
+
+	boolean delete(T o);
+
+	T update(T o);
+
 	T findById(int id);
+
 	List<T> findAll();
-	
 
 }
